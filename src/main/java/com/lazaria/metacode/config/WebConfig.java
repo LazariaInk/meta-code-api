37860297ef7f89acb92ed28dd5497a6a9f405721
Catalina Maria <catalinamariadev@yahoo.com>
2024-05-13 22:09:13 +0300

    set default value in db

--- src/main/java/com/lazaria/metacode/config/WebConfig.java
+++ src/main/java/com/lazaria/metacode/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.fabricadecoduri.com/")
+                .allowedOrigins("https://www.fabricadecoduri.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
