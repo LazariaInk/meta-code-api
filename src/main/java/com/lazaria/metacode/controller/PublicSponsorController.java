5970e5e55761c927c9a843ad747d176e401fd9b4
Catalina Maria <catalinamariadev@yahoo.com>
2024-05-25 17:27:22 +0300

    add registration

--- src/main/java/com/lazaria/metacode/controller/PublicSponsorController.java
+++ src/main/java/com/lazaria/metacode/controller/PublicSponsorController.java
@@ -1,6 +1,8 @@
 package com.lazaria.metacode.controller;
 
 import com.lazaria.metacode.model.Sponsor;
+import com.lazaria.metacode.service.SponsorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/sponsor")
 public class PublicSponsorController {
+    @Autowired
+    private SponsorService sponsorService;
 
     @GetMapping("/all")
     @CrossOrigin(origins = "http://localhost:3000")
